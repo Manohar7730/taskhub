480b55cc0ab6628d4570b9f6000289d50f426885
pediredla manohar <manohar20century@gamil.com>
2025-11-18 15:05:50 +0530

    Stage V1-0: Backend boilerplate setup complete

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "TaskHub API is running..." });
+});
+
+export default app;
